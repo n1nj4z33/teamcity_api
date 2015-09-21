167c2b969575d4e7b747f9fad2d4adf223e471c4
n1nj4z33 <n1nj4z33@gmail.com>
2015-09-21 23:36:21 +0300

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+'''
+A python wrapper for the Teamcity API
+For more information see https://confluence.jetbrains.com/
+'''
+__author__ = 'Alexander Skiridomov'
+__contact__ = 'n1nj4z33@gmail.com'
+__date__ = '21.09.2015'
