5cd73acf7176d0ecebc0a4a792ebb4fb638c50af
Alexander Skiridomov <n1nj4z33@gmail.com>
2015-09-22 01:16:31 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 '''
 A python wrapper for the Teamcity 9.x REST API
-For more information see https://confluence.jetbrains.com/
+For more information see https://confluence.jetbrains.com/display/TCD9/REST+API
 '''
 __author__ = 'Alexander Skiridomov'
 __contact__ = 'n1nj4z33@gmail.com'
